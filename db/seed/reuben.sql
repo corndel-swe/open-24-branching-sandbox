138283ca7c109b3418d4ddc06130c308b13e0fd5
reuben longley <reuben.longley@clarionhg.com>
2025-01-10 14:31:25 +0000

    Movies 2nd attempt

--- db/seed/reuben.sql
+++ db/seed/reuben.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (username) VALUES
 ('Reuben');
-INSERT INTO movies (title, director, release_year, genre) VALUES ('Scarface', 'Brian De Palma', 1983, 'Crime');
-INSERT INTO movies (title, director, release_year, genre) VALUES ('Top Gun', 'Tony Scott', 1986, 'Action');
-INSERT INTO movies (title, director, release_year, genre) VALUES ('The Godfather', 'Francis Ford Coppola', 1972, 'Crime');
-INSERT INTO movies (title, director, release_year, genre) VALUES ('Platoon', 'Oliver Stone', 1986, 'Action');
-INSERT INTO movies (title, director, release_year, genre) VALUES ('Scent Of A Woman', 'Martin Brest', 1992, 'Drama');
+INSERT INTO movies (title) VALUES ('Top Gun');
+INSERT INTO movies (title) VALUES ('Platoon');
+INSERT INTO movies (title) VALUES ('The Godfather');
+INSERT INTO movies (title) VALUES ('Scent Of A Woman');
+INSERT INTO movies (title) VALUES ('Devils Advocate');
