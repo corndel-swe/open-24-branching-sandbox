26fc18fa907a316acd91b955eaf99d92b3c244ff
GLOBAL\ethan.wicks <ethan.wicks@necsws.com>
2025-01-10 14:58:58 +0000

    add

---
+++ db/seed/favorites.sql
@@ -0,0 +1,3 @@
+INSERT INTO favourites (user_id, movie_id) VALUES ('2', '1');
+INSERT INTO favourites (user_id, movie_id) VALUES ('2', '2');
+INSERT INTO favourites (user_id, movie_id) VALUES ('2', '3');
