14bdbb1778257d87006dbc430c27c691d07a7183
reuben longley <reuben.longley@clarionhg.com>
2025-01-10 15:38:01 +0000

    conflict resolve

--- db/seed/favorites.sql
+++ db/seed/favorites.sql
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 INSERT INTO favourites (user_id, movie_id) VALUES (1, 1);
 INSERT INTO favourites (user_id, movie_id) VALUES (1, 2);
 INSERT INTO favourites (user_id, movie_id) VALUES (1, 3);
-=======
 INSERT INTO favourites (user_id, movie_id) VALUES ('2', '1');
 INSERT INTO favourites (user_id, movie_id) VALUES ('2', '2');
 INSERT INTO favourites (user_id, movie_id) VALUES ('2', '3');
@@ -10,4 +8,3 @@ INSERT INTO favourites (user_id, movie_id) VALUES (2,2);
 INSERT INTO favourites (user_id, movie_id) VALUES (2,4);
 INSERT INTO favourites (user_id, movie_id) VALUES (2,6);
 
->>>>>>> 7814c66ff3ed670631c05f7b6d811dc3ecb38a06
